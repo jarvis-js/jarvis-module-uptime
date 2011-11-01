20e09c2e0a269385c71195908b9bfb3b097ff7b4
Alex Whitman <alex@alexwhitman.com>
2011-11-01 19:57:49 +0000

    Add uptime module

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = function(bot) {
+
+	uptimeModule = new bot.Module();
+
+	uptimeModule.load = function() {
+
+		this.start = new Date().getTime();
+		var self = this;
+
+		bot.register(this.name, 'uptime', function(request, response) {
+			var now = new Date().getTime();
+			var uptime_seconds = Math.floor((now - self.start) / 1000);
+			var intervals = {};
+			intervals.day = Math.floor(uptime_seconds / 86400);
+			intervals.hour = Math.floor((uptime_seconds % 86400) / 3600);
+			intervals.minute = Math.floor(((uptime_seconds % 86400) % 3600) / 60);
+			intervals.second = ((uptime_seconds % 86400) % 3600) % 60;
+
+			var elements = []
+			for (var interval in intervals) {
+				var value = intervals[interval];
+				if (value > 0) {
+					elements.push(value + ' ' + interval + self.numPlural(value));
+				}
+			}
+
+			var reply = '';
+			if (elements.length > 1) {
+				var last = elements.pop();
+				reply = elements.join(', ');
+				reply += ' and ' + last;
+			}
+			else {
+				reply = elements.join(', ');
+			}
+
+			response('I\'ve been sentient for ' + reply);
+		});
+	};
+
+	uptimeModule.numPlural = function(num) {
+		return (num != 1) ? 's' : '';
+	};
+
+	return uptimeModule;
+}
