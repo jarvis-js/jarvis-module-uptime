911c8f605584de8bb7bbb517e81287cabe803198
Alex Whitman <alex@alexwhitman.com>
2011-11-18 16:24:15 +0000

    Remove bot.Module.load() - no longer required
    - options are now assigned to module rather than passed in

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
 module.exports = function(bot, module) {
 
-	module.load = function() {
+	module.start = new Date().getTime();
 
-		module.start = new Date().getTime();
+	module.addCommand('uptime', function(request) {
+		var now = new Date().getTime();
+		var uptime_seconds = Math.floor((now - module.start) / 1000);
+		var intervals = {};
+		intervals.day = Math.floor(uptime_seconds / 86400);
+		intervals.hour = Math.floor((uptime_seconds % 86400) / 3600);
+		intervals.minute = Math.floor(((uptime_seconds % 86400) % 3600) / 60);
+		intervals.second = ((uptime_seconds % 86400) % 3600) % 60;
 
-		module.addCommand('uptime', function(request) {
-			var now = new Date().getTime();
-			var uptime_seconds = Math.floor((now - module.start) / 1000);
-			var intervals = {};
-			intervals.day = Math.floor(uptime_seconds / 86400);
-			intervals.hour = Math.floor((uptime_seconds % 86400) / 3600);
-			intervals.minute = Math.floor(((uptime_seconds % 86400) % 3600) / 60);
-			intervals.second = ((uptime_seconds % 86400) % 3600) % 60;
-
-			var elements = []
-			for (var interval in intervals) {
-				var value = intervals[interval];
-				if (value > 0) {
-					elements.push(value + ' ' + interval + module.numPlural(value));
-				}
+		var elements = []
+		for (var interval in intervals) {
+			var value = intervals[interval];
+			if (value > 0) {
+				elements.push(value + ' ' + interval + module.numPlural(value));
 			}
+		}
 
-			var reply = '';
-			if (elements.length > 1) {
-				var last = elements.pop();
-				reply = elements.join(', ');
-				reply += ' and ' + last;
-			}
-			else {
-				reply = elements.join(', ');
-			}
+		var reply = '';
+		if (elements.length > 1) {
+			var last = elements.pop();
+			reply = elements.join(', ');
+			reply += ' and ' + last;
+		}
+		else {
+			reply = elements.join(', ');
+		}
 
-			request.reply = 'I\'ve been sentient for ' + reply;
-			bot.respond(request);
-		});
-	};
+		request.reply = 'I\'ve been sentient for ' + reply;
+		bot.respond(request);
+	});
 
 	module.numPlural = function(num) {
 		return (num != 1) ? 's' : '';
